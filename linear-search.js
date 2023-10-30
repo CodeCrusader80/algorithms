41d9340e7560dd9c059d844ee1640ed049e32105
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-10-30 15:54:11 +0100

    linear search algo

---
+++ linear-search.js
@@ -0,0 +1,10 @@
+//Simple implementation of Linear Search. It is probably one of the simplest search algorithms to understand and implement.
+
+function linearSearch(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i;
+        }
+    }
+    return -1;
+}
